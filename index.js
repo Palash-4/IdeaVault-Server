403e1b31de37b0d0f9c25178b6c9204b0d98333d
Palash-4 <hasanalltarek9658@gmail.com>
2026-05-19 00:45:56 +0600

    Comment section up to complete

---
+++ index.js
@@ -0,0 +1,125 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const cors = require("cors")
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+dotenv.config()
+
+
+
+
+const uri = process.env.MONGODB_URI;
+
+const app = express()
+const PORT = process.env.PORT;
+
+app.use(cors())
+app.use(express.json())
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+async function run() {
+    try {
+        await client.connect();
+
+        const db = client.db("ideavault")
+        const ideaCollection = db.collection("ideas")
+
+        const commentCollection =
+            db.collection("comments");
+
+        app.get('/ideas', async (req, res) => {
+            const result = await ideaCollection.find().toArray()
+            res.json(result);
+
+        })
+
+        app.post('/ideas', async (req, res) => {
+            const ideaData = req.body
+            const result = await ideaCollection.insertOne(ideaData)
+
+            res.json(result)
+        })
+
+        app.get('/ideas/:id', async (req, res) => {
+            const { id } = req.params;
+            const result = await ideaCollection.findOne({ _id: new ObjectId(id) })
+            res.json(result)
+
+        })
+
+        app.post("/comments", async (req, res) => {
+            const commentData = req.body;
+            const result =
+                await commentCollection.insertOne(
+                    commentData
+                );
+
+            res.json(result);
+        });
+
+        app.get("/comments/:ideaId", async (req, res) => {
+
+            const { ideaId } = req.params;
+            const result =
+                await commentCollection
+                    .find({ ideaId })
+                    .toArray();
+
+            res.json(result);
+        });
+
+        app.delete("/comments/:id", async (req, res) => {
+
+            const { id } = req.params;
+
+            const result =
+                await commentCollection.deleteOne({
+                    _id: new ObjectId(id),
+                });
+
+            res.json(result);
+        });
+
+        app.patch("/comments/:id", async (req, res) => {
+
+            const { id } = req.params;
+
+            const { comment } = req.body;
+
+            const result =
+                await commentCollection.updateOne(
+                    {
+                        _id: new ObjectId(id),
+                    },
+                    {
+                        $set: {
+                            comment,
+                        },
+                    }
+                );
+
+            res.json(result);
+        });
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send("Server Is Running")
+})
+
+app.listen(PORT, () => {
+    console.log(`server is running${PORT}`);
+
+})
